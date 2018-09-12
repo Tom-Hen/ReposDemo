9771f8663a6d3b172ba357bf76f4ece38dca0f7a
Tom Hennessy <tom.hennessy@storm12.co.uk>
2018-09-12 12:48:22 +0100

    Base content

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ReposDemo
 This is a demo for showing how repositories work
+The description text has a horrid red background which i plan to change soon.
---
+++ index.php
@@ -0,0 +1,4 @@
+<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+<h1>Welcome to my website!!</h1>
+<p class="description">This is my website so that i can demo how git works :)</p>
+<a href="https://github.com/Tom-Hen/ReposDemo/blob/master/README.md">Check out my project here!</a>
